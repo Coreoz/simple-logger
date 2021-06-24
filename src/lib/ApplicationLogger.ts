622131cbbc8d9de7b08adf850b94f5e6c91824a3
Aurélien Manteaux <amanteaux@coreoz.com>
2021-06-24 11:33:54 +0200

    Export logging function as a static member

--- src/lib/ApplicationLogger.ts
+++ src/lib/ApplicationLogger.ts
@@ -4,27 +4,27 @@ export interface LoggerFunction {
   (level: LoggerLevel, loggerName: string, message: string, args: any[]): void;
 }
 
-export const loggerConsole = (
-  level: LoggerLevel, loggerName: string, message: string, args: any[],
-) => {
-  const fullMessage = `${loggerName}: ${message}`;
-  let consoleFunction = console.error;
-  if (level === LoggerLevel.DEBUG) {
-    consoleFunction = console.debug;
-  } else if (level === LoggerLevel.INFO) {
-    consoleFunction = console.info;
-  } else if (level === LoggerLevel.WARN) {
-    consoleFunction = console.warn;
-  }
-  if (args.length === 0) {
-    consoleFunction.call(console, fullMessage);
-  } else {
-    consoleFunction.call(console, fullMessage, args);
-  }
-};
-
 export class ApplicationLogger {
-  private static loggerFunction: LoggerFunction = loggerConsole;
+  private static loggerFunction: LoggerFunction = ApplicationLogger.loggerConsole;
+
+  static loggerConsole(
+    level: LoggerLevel, loggerName: string, message: string, args: any[],
+  ) {
+    const fullMessage = `${loggerName}: ${message}`;
+    let consoleFunction = console.error;
+    if (level === LoggerLevel.DEBUG) {
+      consoleFunction = console.debug;
+    } else if (level === LoggerLevel.INFO) {
+      consoleFunction = console.info;
+    } else if (level === LoggerLevel.WARN) {
+      consoleFunction = console.warn;
+    }
+    if (args.length === 0) {
+      consoleFunction.call(console, fullMessage);
+    } else {
+      consoleFunction.call(console, fullMessage, args);
+    }
+  }
 
   static setLoggerFunction(loggerFunction: LoggerFunction) {
     ApplicationLogger.loggerFunction = loggerFunction;
